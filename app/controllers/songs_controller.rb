993624aab5da02ae31353bb24210f9b457e2c0c7
MokumJ <jesse.vanderheijden@gmail.com>
2017-11-19 18:19:50 +0100

    working on jquery and view

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -20,7 +20,7 @@ before_action :set_song, only: [:show, :edit, :update, :destroy]
       if  @song.save!
 
         format.html { redirect_to artist_path(@artist) }
-        format.json { render :show, status: :created, location: @artist }
+        format.json { render :show, status: :created, location: @song }
 
     else
       format.html { redirect_to @artist }
